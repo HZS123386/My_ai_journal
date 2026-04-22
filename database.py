6c045179e377c344433dcc8ec036dc6aa4905256
1063024943 <1063024943@qq.com>
2026-04-22 14:42:09 +0800

    use postgres database url

--- database.py
+++ database.py
@@ -1,27 +1,20 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("sqlite:///./journal.db")
+DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./journal.db"
 
-engine = create_engine(   #Python和数据库之间的总连接器
+# 兼容某些平台提供的 postgres:// 前缀
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
     DATABASE_URL,
-    connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}   #允许这个连接在不同线程里被使用
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-                            #autocommit=False   不会自动提交，你要手动 db.commit()
-                            #autoflush=False    不会自动把内存里的改动立刻同步到数据库
-
 Base = declarative_base()
-
-
-
-
-
-
-
-
